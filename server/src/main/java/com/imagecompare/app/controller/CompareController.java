db6156a5d4bf4901a155417f1b55489aa2f19955
Saul Osorio <saul.osorio@solera.com>
2023-03-21 17:04:01 +0100

    Changed Compare controller to get a request body

--- server/src/main/java/com/imagecompare/app/controller/CompareController.java
+++ server/src/main/java/com/imagecompare/app/controller/CompareController.java
@@ -3,10 +3,12 @@ package com.imagecompare.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.imagecompare.app.model.Names;
 import com.imagecompare.app.service.CompareService;
 
 @RestController
@@ -16,9 +18,10 @@ public class CompareController {
 	CompareService compareService;
 	
 	
-	@GetMapping("/compare")
-	public ResponseEntity<String> getComparisonResult(@RequestParam("baseImageName") String baseImageName,
-            @RequestParam("compareImageName") String compareImageName){
+	@PostMapping("/compare")
+	public ResponseEntity<String> getComparisonResult(@RequestBody Names names){
+		String baseImageName = names.getBaseImageName();
+		String compareImageName = names.getCompareImageName();
 		try {
 			System.out.println(baseImageName);
 			 System.out.println(compareImageName);
