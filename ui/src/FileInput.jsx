386506c73d0331cbe83d396978987f13ab8e11d7
Fabian Mejia <Bryan.mejia@solera.com>
2023-03-28 14:31:16 +0100

    FrontChanges

--- ui/src/FileInput.jsx
+++ ui/src/FileInput.jsx
@@ -3,7 +3,9 @@ import './style.css';
 
 export const FileInput = ({ id, name, accept, onChange }) => {
   return (
+    
     <input type="file" className="custom-file-input" id={id} name={name} accept={accept} onChange={onChange} />
+    
   );
 };
 
